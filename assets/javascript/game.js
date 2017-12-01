e7cf32f419a6deda0147d52619118e4fb52f13b5
Bob Brown <robertbernardbrown@gmail.com>
2017-12-01 15:52:07 -0500

    initial Javascript to hide the main content area where the game is played (#bodyContent)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,10 @@
-Placeholder
+//grab triggers
+var bodyContent = document.getElementById("bodyContent");
+
+console.log(bodyContent);
+
+
+//event listeners
+window.onload = function() {
+    bodyContent.style.display = 'none';
+  };
