0f7535814c6e7f31906deeacdd9a33ceccb2131a
Bob Brown <robertbernardbrown@gmail.com>
2017-12-01 17:34:58 -0500

    Make 'tap any key' prompt dissappear onkeydown and make content area appear on keydown by adding and removing classes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,19 @@
 //grab triggers
 var bodyContent = document.getElementById("bodyContent");
+var prompt = document.getElementById("directions");
 
 console.log(bodyContent);
 
+function showContent(e) {
+    bodyContent.classList.add("container", "body");
+    bodyContent.classList.remove("hidden");
+}
+
+function hidePrompt (e) {
+    prompt.classList.add("hidden");
+    prompt.classList.remove("title", "container");
+}
 
 //event listeners
-window.onload = function() {
-    bodyContent.style.display = 'none';
-  };
+document.addEventListener('keydown', showContent)
+document.addEventListener('keydown', hidePrompt)
