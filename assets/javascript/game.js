5eddefbbe31d1f3634f8a3c2804ffcb25b772b06
Bob Brown <robertbernardbrown@gmail.com>
2017-12-02 13:19:50 -0500

    add crab to guess words

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,8 @@ var prompt = document.getElementById("directions");
 var userGuess = document.getElementById("guessed");
 var letterArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var blankArray = [];
+var userChances = document.getElementById("chances");
+var hangmanWords = ["hawaii", "surfing", "Humuhumunukunukuapuaa", "luau", "pineapple", "beach", "coconut", "crab"]
 
 function showContent(e) {
     bodyContent.classList.add("container", "body");
@@ -26,7 +28,14 @@ function userInput (e) {
     }
 }
 
+//COME BACK TO THIS ONE
+function updateChances (e) {
+    //userChances.innerHTML = 11;
+
+}
+
 //event listeners
 document.addEventListener('keydown', showContent)
 document.addEventListener('keydown', hidePrompt)
 document.addEventListener('keydown', userInput);
+document.addEventListener('keydown', updateChances);
